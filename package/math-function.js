fc80f12b3fe287918c685faf55608602a915b8d9
Nurima Lumbantoruan <126258902+nurima3001@users.noreply.github.com>
2023-04-10 11:10:40 +0700

    Add files via upload

---
+++ package/math-function.js
@@ -0,0 +1,24 @@
+exports.add = function add(a,b) {
+    return a + b;
+}
+exports.sub = function sub(a ,b) {
+    return a - b;
+}
+exports.mul = function mul(a,b) {
+    return a * b;
+}
+exports.div = function div(a,b) {
+    return a / b;
+}
+
+//exports.add = add;
+//exports.sub = sub;
+//exports.mul = mul;
+//exports.div = div;
+
+// export langsung
+// module.exports = {
+//  add,
+//  sub,
+//  mul,
+//  div ,  
